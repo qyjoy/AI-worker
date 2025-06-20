3eea0184756da3d540a028268826ffd612ef8b3a
QyJoy <85855855+qyjoy@users.noreply.github.com>
2025-06-21 05:19:20 +0800

    Update api-handler.js

--- V4 EX(For mobile support Expand)/api-handler.js
+++ V4 EX(For mobile support Expand)/api-handler.js
@@ -24,8 +24,8 @@ export async function handleApiCall(request, env, logPrefix) {
 
   const allowedModels = [
     "gemini-2.0-flash",
-    "gemini-2.5-flash-preview-04-17",
-    "gemini-2.5-pro-preview-05-06",
+    "gemini-2.5-flash-preview-05-20",
+    "gemini-2.5-pro-preview-06-05",
     "gemini-2.0-flash-preview-image-generation",
     "veo-2.0-generate-001"
   ];
